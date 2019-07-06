61bbddc7ed3b3fbf29a399db317ea9d526b818ca
Antonio Fry <antoniofry2000@gmail.com>
2019-07-06 14:12:46 -0600

    add app testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, mount } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+
+  var wrapper;
+  beforeEach(function() {
+    wrapper = shallow(<App />);
+  });
+
+  it('should match the snapshot with the data being passed in correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  // it('should update state when fetchData is invoked', () => {
+
+  //   wrapper.instance().fetchData('people');
+
+  //   expect(wrapper.state('people').length).toEqual(10);
+  // });
+
+})
