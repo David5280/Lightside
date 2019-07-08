305eab3502c9f78f0ef6647d07eb548a81fbfea5
Antonio Fry <antoniofry2000@gmail.com>
2019-07-08 17:19:02 -0600

    start testing fetchData method

--- src/apicalls/apicalls.js
+++ src/apicalls/apicalls.js
@@ -1,7 +1,8 @@
 const fetchData = (string, context) => {
   const data = fetch(`https://swapi.co/api/${string}/`)
     .then(res => res.json())
-    .then(data => context.setState({ [string]: data.results }))
+    .then(data => data.results)
+    .then(results => context.setState({ [string]: results }))
     .catch(error => context.setState({ error: error }));
   return data
 }
